b5fe75d0394152a65e976b20dbcc9c8fe0e272df
Weiqing Yang <yangweiqing001@gmail.com>
2017-02-20 21:38:04 -0800

    Add an new assertion in test

--- core/src/test/scala/org/apache/spark/sql/PhoenixSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/PhoenixSuite.scala
@@ -114,9 +114,10 @@ class PhoenixSuite extends SHC with Logging {
   test("IN and Not IN filter1") {
     val df = withCatalog(catalog)
     val s = df.filter(($"col0" isin ("row005", "row001", "row002")) and !($"col0" isin ("row001", "row002")))
-      .select("col0")
+      .select("col0", "col8")
     s.explain(true)
     s.show
     assert(s.count() == 1)
+    assert(s.first().getByte(1) == 5)
   }
 }
