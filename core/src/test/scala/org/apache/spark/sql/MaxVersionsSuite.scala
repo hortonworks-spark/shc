4f5b398bca95f3c86856cb409de5675bc0995efe
Weiqing Yang <yangweiqing001@gmail.com>
2017-11-02 13:39:18 -0700

    update licence header for MaxVersionsSuite file

--- core/src/test/scala/org/apache/spark/sql/MaxVersionsSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/MaxVersionsSuite.scala
@@ -1,8 +1,7 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (C) 2017 Hortonworks, Inc. All rights reserved. See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership. This file is licensed to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -13,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * File modified by Hortonworks, Inc. Modifications are also licensed under
- * the Apache Software License, Version 2.0.
  */
 
 package org.apache.spark.sql
