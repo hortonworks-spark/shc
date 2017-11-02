0d22c4a4403427967ce49ce25c1009dac226d0ef
Bartłomiej Tomala <btomala@virtuslab.com>
2017-11-02 11:41:56 +0100

    Cleaned & added comments

--- core/src/test/scala/org/apache/spark/sql/MaxVersionsSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/MaxVersionsSuite.scala
@@ -22,106 +22,82 @@ package org.apache.spark.sql
 
 import org.apache.spark.sql.execution.datasources.hbase.Logging
 import org.apache.spark.sql.execution.datasources.hbase.{HBaseRelation, HBaseTableCatalog}
-import org.apache.spark.sql.sources.PrunedFilteredScan
-
 
 class MaxVersionsSuite extends SHC with Logging {
 
-  def withCatalog(cat: String): DataFrame = {
-    sqlContext
-      .read
-      .options(Map(HBaseTableCatalog.tableCatalog->cat))
+  def withCatalog(cat: String, options: Map[String,String]): DataFrame = {
+    sqlContext.read
+      .options(options ++ Map(HBaseTableCatalog.tableCatalog -> catalog))
       .format("org.apache.spark.sql.execution.datasources.hbase")
       .load()
   }
 
-  private def prunedFilterScan(cat: String): PrunedFilteredScan = {
-    HBaseRelation(Map(HBaseTableCatalog.tableCatalog->cat),None)(sqlContext)
+  def persistDataInHBase(cat: String, data: Seq[HBaseRecord], timestamp: Long): Unit = {
+    val sql = sqlContext
+    import sql.implicits._
+    sc.parallelize(data).toDF.write
+      .options(Map(
+        HBaseTableCatalog.tableCatalog -> cat,
+        HBaseTableCatalog.newTable -> "5",
+        HBaseRelation.MAX_VERSIONS -> "3",
+        HBaseRelation.TIMESTAMP -> timestamp.toString
+      ))
+      .format("org.apache.spark.sql.execution.datasources.hbase")
+      .save()
   }
 
   test("Max Versions semantics") {
-    val sql = sqlContext
-    import sql.implicits._
 
     val oldestMs = 754869600000L
     val oldMs = 754869611111L
     val newMs = 754869622222L
     val newestMs = 754869633333L
 
-    val oldestData = (0 to 2).map { i =>
-      HBaseRecord(i, "ancient")
-    }
-    val oldData = (0 to 2).map { i =>
-      HBaseRecord(i, "old")
-    }
-    val newData = (0 to 2).map { i =>
-      HBaseRecord(i, "new")
-    }
-    val newestData = (0 to 1).map { i =>
-      HBaseRecord(i, "latest")
-    }
+    val oldestData = (0 to 2).map(HBaseRecord(_, "ancient"))
+    val oldData = (0 to 2).map(HBaseRecord(_, "old"))
+    val newData = (0 to 2).map(HBaseRecord(_, "new"))
+    val newestData = (0 to 1).map(HBaseRecord(_, "latest"))
 
-    sc.parallelize(oldestData).toDF.write.options(
-      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5", HBaseRelation.MAX_VERSIONS -> "3", HBaseRelation.TIMESTAMP -> oldestMs.toString))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .save()
-    sc.parallelize(oldData).toDF.write.options(
-      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5", HBaseRelation.MAX_VERSIONS -> "3", HBaseRelation.TIMESTAMP -> oldMs.toString))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .save()
-    sc.parallelize(newData).toDF.write.options(
-      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5", HBaseRelation.MAX_VERSIONS -> "3", HBaseRelation.TIMESTAMP -> newMs.toString))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .save()
-    sc.parallelize(newestData).toDF.write.options(
-      Map(HBaseTableCatalog.tableCatalog -> catalog, HBaseTableCatalog.newTable -> "5", HBaseRelation.MAX_VERSIONS -> "3", HBaseRelation.TIMESTAMP -> newestMs.toString))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .save()
+    persistDataInHBase(catalog, oldestData, oldestMs)
+    persistDataInHBase(catalog, oldData, oldMs)
+    persistDataInHBase(catalog, newData, newMs)
+    persistDataInHBase(catalog, newestData, newestMs)
 
     // Test specific last two versions
-    val twoVersions: DataFrame = sqlContext.read
-      .options(Map(HBaseTableCatalog.tableCatalog->catalog, HBaseRelation.MAX_VERSIONS -> "2", HBaseRelation.MERGE_TO_LATEST -> "false"))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .load()
+    val twoVersions: DataFrame = withCatalog(catalog, Map(
+      HBaseRelation.MAX_VERSIONS -> "2",
+      HBaseRelation.MERGE_TO_LATEST -> "false"
+    ))
 
     //count is made on HBase directly and return number of unique rows
     assert(twoVersions.count() == 3)
 
     val rows = twoVersions.take(10)
     assert(rows.size == 6)
-    assert(rows.filter(row => row.getString(7).contains("ancient")).size == 0)
-    assert(rows.filter(row => row.getString(7).contains("old")).size == 1)
-    assert(rows.filter(row => row.getString(7).contains("new")).size == 3)
-    assert(rows.filter(row => row.getString(7).contains("latest")).size == 2)
+    assert(rows.count(_.getString(7).contains("ancient")) == 0)
+    assert(rows.count(_.getString(7).contains("old")) == 1)
+    assert(rows.count(_.getString(7).contains("new")) == 3)
+    assert(rows.count(_.getString(7).contains("latest")) == 2)
 
     //we cannot take more then three because we create table with that size
-    val threeVersions: DataFrame = sqlContext.read
-      .options(Map(HBaseTableCatalog.tableCatalog->catalog, HBaseRelation.MAX_VERSIONS -> "4", HBaseRelation.MERGE_TO_LATEST -> "false"))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .load()
-
-    //count is made on HBase directly and return number of unique rows
-    assert(threeVersions.count() == 3)
+    val threeVersions: DataFrame = withCatalog(catalog, Map(
+      HBaseRelation.MAX_VERSIONS -> "4",
+      HBaseRelation.MERGE_TO_LATEST -> "false"
+    ))
 
     val threeRows = threeVersions.take(10)
     assert(threeRows.size == 9)
-    assert(threeRows.filter(row => row.getString(7).contains("ancient")).size == 1)
-    assert(threeRows.filter(row => row.getString(7).contains("old")).size == 3)
-    assert(threeRows.filter(row => row.getString(7).contains("new")).size == 3)
-    assert(threeRows.filter(row => row.getString(7).contains("latest")).size == 2)
+    assert(threeRows.count(_.getString(7).contains("ancient")) == 1)
+    assert(threeRows.count(_.getString(7).contains("old")) == 3)
+    assert(threeRows.count(_.getString(7).contains("new")) == 3)
+    assert(threeRows.count(_.getString(7).contains("latest")) == 2)
 
     // Test specific only last versions
-    val lastVersions: DataFrame = sqlContext.read
-      .options(Map(HBaseTableCatalog.tableCatalog->catalog))
-      .format("org.apache.spark.sql.execution.datasources.hbase")
-      .load()
+    val lastVersions: DataFrame = withCatalog(catalog, Map.empty)
 
     val lastRows = lastVersions.take(10)
     assert(lastRows.size == 3)
-    assert(lastRows.filter(row => row.getString(7).contains("new")).size == 1)
-    assert(lastRows.filter(row => row.getString(7).contains("latest")).size == 2)
-
-
+    assert(lastRows.count(_.getString(7).contains("new")) == 1)
+    assert(lastRows.count(_.getString(7).contains("latest")) == 2)
   }
-
 }
